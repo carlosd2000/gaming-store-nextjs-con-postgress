d764bf55f750f20131aac4b2dbe511584ef1d24a
carlosd2000 <caerlosdavid29@gmail.com>
2024-12-17 02:55:23 -0500

    variable del entorno agregada

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,14 +1,7 @@
-// importar libreria para la conexxion de postgres
-
 import { Pool } from "pg";
 
-// crear la conexion a la base de datos postgresSQL
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'crud_users',
-    password: '123456789',
-    port: 5432
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
